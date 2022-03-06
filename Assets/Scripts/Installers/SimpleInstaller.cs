e98e25ffd7e0acd24c3b4ffa3e21b8e2a89c1a20
Serge Craft <alexandrsudakov@tutanota.com>
2022-03-06 10:53:46 +0300

    Set up basic zenject installer and scene context

---
+++ Assets/Scripts/Installers/SimpleInstaller.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class SimpleInstaller : MonoInstaller
+{
+    public override void InstallBindings()
+    {
+        
+    }
+}
